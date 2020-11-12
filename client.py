8305d2dc52e881017b9bcdeb5dbd834303697460
amyl1 <amyvlaws@gmail.com>
2020-11-12 09:56:12 +0000

    New version

--- client.py
+++ client.py
@@ -1,26 +1,21 @@
 import socket 
 import sys 
-
+import time
         
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 username = str(sys.argv[1])
 hostName = str(sys.argv[2]) 
-Port = int(sys.argv[3]) 
-server.connect((hostName, Port)) 
-msg = server.recv(1024)
-print(msg.decode())
-server.send('Server: {} has joined the chat. Say hello!'.format(username).encode())
+port = int(sys.argv[3])
+
+server=socket.socket()
+server.connect((hostName,port))
+print('Connected to the chat server')
 
 while True: 
-    #buffer, how large chuncks of data do we want to receive at once. 
-    msg = server.recv(1024)
-    print(msg.decode())
-    usermessage=str(input("Enter your message:"))
-    if usermessage != "QUIT":
-        usermessage=username+": "+usermessage
-        server.send(usermessage.encode())
-    else:
-        #quit
-        server.send("QUIT".encode())
-        #send message to say leaving
-        server.close() 
+   incoming_message=server.recv(1024)
+   incoming_message=incoming_message.decode()
+   print(" Server :", incoming_message)
+   message= input(str("Enter a message:"))
+   message =message.encode()
+   server.send(message)
+   print(" message has been sent...")
--- server.py
+++ server.py
@@ -1,40 +1,23 @@
+import socket  
+import sys
+import time
 
-import socket 
-import select 
-import sys 
+server=socket.socket()
+port = int(sys.argv[1])
+server.bind(('',port))
+print("server done binding to host and port successfully")
+print("Waiting for connections...")
+#maximum of 20 connections
+server.listen(20)
+connection,address= server.accept()
+#replace with username
 
-def broadcast(users,new_message):
-    for user in users:
-        user.send(f": {new_message}".encode())
+display_mess= (" {} has connected to the server and is now online...".format(address))
+display_mess=display_mess.encode()
+connection.send(display_mess)
+print(" message has been sent...")
+while True: 
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-  
-
-# local host
-hostName = ''
-  
-# takes second argument from command prompt as port number 
-Port = int(sys.argv[1]) 
-server.bind((hostName, Port)) 
-  
-
-
-server.listen(100) 
-
-current_users=[]
-while True:
-    clientsocket, address = server.accept()
-    current_users.append(clientsocket)
-    print(f"Connection from {address} has been established")
-    clientsocket.send("Welcome to the server! \n To quit the server, type QUIT \n".encode())
-    message = clientsocket.recv(1024) 
-    if message:
-        if message!="QUIT":
-            broadcast(current_users,message)
-        else: #replace with username
-            broadcast(current_users,"User has left")
-            break
-
-conn.close() 
-server.close() 
+   in_message=connection.recv(1024)
+   in_messagge=in_message.decode()
+   print(in_message)
