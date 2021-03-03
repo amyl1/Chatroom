7091dc7a71e8ee3ca2e007cefa55e7e10ea6b771
amyl1 <amyvlaws@gmail.com>
2021-03-03 08:41:21 +0000

    Final commit

--- Task2/client.py
+++ Task2/client.py
@@ -1,7 +1,6 @@
 #check server message if user leaves
 import socket
 import sys 
-import time
 
 import tkinter as tk
 
@@ -12,21 +11,44 @@ from tkinter import Radiobutton
 
 def showAllUsers():
     msg="DISPLAY"
-    msg_list.insert(tk.END, "Current Users:")
+    msgList.insert(tk.END, "Current Users:")
     client_socket.send(msg.encode())
     
 
 def helpFunc():
-    messagebox.showinfo("Title", "a Tk MessageBox")
+    #Generate a pop-up window to display commands.
+    popUp = tk.Tk()
+    popUp.title("Help")
+    popUp.geometry(f'{500}x{300}')
+    m = """ 
+        Welcome to the server!\n
+        There is a menu containing the options to change your username,\n
+        list all users, leave the chat or ask for help.\n
+        Radio buttons are used to select whom you wish to send the message.\n
+        If you select 'whisper', there will then be an input prompt\n
+        where you type the name of the user you will send the message to.\n
+        To leave the chat, you can either send the message'quit' or use the menubar.\n
+        Messages cannot contain the '*' character as this is reserved by the server.
+        """
+    w = tk.Label(popUp, text=m, width=300, height=17)
+    w.pack()
+    b = tk.Button(popUp, text="OK", command=popUp.destroy, width=10)
+    b.pack()
+    tk.mainloop()
 
 def changeName():
-    name = simpledialog.askstring("Change Username", "Please enter your new name")
+    name=""
+    while not name:
+        name = simpledialog.askstring("Change Username", "Please enter your new username")
+    #send new username to server
     msg="USER"+name
     client_socket.send(msg.encode())
-
+    global username
+    username=name
 
 def leaveFunc():
     messagebox.showinfo("Leave Chat", "You have left the chat")
+    #disconnect the user
     msg="quit"
     client_socket.send(msg.encode())
 
@@ -34,32 +56,45 @@ def receive():
     while True:
         try:
             msg = client_socket.recv(1024).decode()
+            #if user is leaving the server
             if msg=="quit":
-                msg_list.insert(tk.END, "You have left the server")
-                client_socket.close()
+                msgList.insert(tk.END, "You have left the server. You can now close the window")
                 window.quit()
+                client_socket.close()
                 break
+            #add recieved message to message list
             else:                
-                msg_list.insert(tk.END, msg)
+                msgList.insert(tk.END, msg)
         except:
-            msg_list.insert(tk.END, "Error. Please restart.")
+            msgList.insert(tk.END, "Error. Please restart.")
+            window.quit()
             break
    
 def send():
-    if Radio_Value0.get () == 1:
-        recp="ALL"
-    else:
-        recp = simpledialog.askstring("Choose User", "Enter a username")
-    msg=username+": "+msgForm.get() +"*"+recp
+    #get value from radio buttons and send to requested recipient
+    msg=msgForm.get()
     msgForm.set("")
-    client_socket.send(msg.encode())
+    if '*' in msg:
+        msg="Messages cannot contain '*', please resend the message."
+        msgList.insert(tk.END, msg)
+    else:
+        if radioValue.get () == 1:
+            recp="ALL"
+        else:
+            recp=""
+            while not recp:
+                recp = simpledialog.askstring("Choose User", "Enter a username")
+        msg=username+": "+msg +"*"+recp
+        client_socket.send(msg.encode())
 
+#set up tkinter window
 window=tk.Tk()
 window.title("Instant Messenger")
 width  = window.winfo_screenwidth()
 height = window.winfo_screenheight()
-window.geometry(f'{width}x{height}')
+window.geometry(f'{width//2}x{height//2}')
 
+#set up menu
 menubar=tk.Menu(window)
 
 nameMenu=tk.Menu(menubar, tearoff=0)
@@ -79,24 +114,29 @@ leaveMenu.add_command(label="Leave the chat",command=leaveFunc)
 menubar.add_cascade(label="Leave", menu=leaveMenu)
 
 window.config(menu=menubar)
-Radio_Value0 = tk.IntVar ()
-Radiobutton(window, text = "Send to All", variable = Radio_Value0, value = 1).grid(column=0, row=1)
-Radiobutton(window, text = "Whisper", variable = Radio_Value0,value = 2).grid(column=1, row=1)
 
+#radio buttons, to select recipient
+radioValue = tk.IntVar ()
+radioValue.set(1)
+Radiobutton(window, text = "Send to All", variable = radioValue, value = 1).grid(column=0, row=1)
+Radiobutton(window, text = "Whisper", variable = radioValue,value = 2).grid(column=1, row=1)
+
+#to display recieved messages
 messages_frame=tk.Frame(window)
 msgForm=tk.StringVar()
 msgForm.set("Enter message: ")
 scrollbar = tk.Scrollbar(messages_frame)
-msg_list = tk.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+msgList = tk.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y, expand=False)
-msg_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-msg_list.pack()
+msgList.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+msgList.pack()
 messages_frame.grid(row=4)
 
-entry_field = tk.Entry(window, textvariable=msgForm)
-entry_field.grid(column=0, row=5)
-send_button = tk.Button(window, text="Send", command=send)
-send_button.grid(column=1, row=5)
+#form to enter message to send
+entryField = tk.Entry(window, textvariable=msgForm)
+entryField.grid(column=0, row=5)
+sendButton = tk.Button(window, text="Send", command=send)
+sendButton.grid(column=1, row=5)
 
 window.columnconfigure(0, weight=1)
 window.columnconfigure(1, weight=1)
@@ -109,7 +149,8 @@ try:
     port = int(sys.argv[3])
 except ValueError:
     print ('Type error: Incorrect command line arguments given')
-    sys.exit(1) 
+    sys.exit(1)
+
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 username = str(sys.argv[1])
 hostName = str(sys.argv[2]) 
@@ -117,13 +158,16 @@ port = int(sys.argv[3])
 
 while not username:
    username=input("Please enter a non-empty username")
+#connect client to server
 try:
    client_socket.connect((hostName,port))
 except socket.error:
    sys.exit("Error connecting: please check the port and hostname")
-msg_list.insert(tk.END, 'Welcome! If you want to quit, type quit to exit.')
+
+msgList.insert(tk.END, 'Welcome! If you want to quit, type quit to exit.')
+
 receive_thread = Thread(target=receive)
 receive_thread.start()
 client_socket.send(username.encode())
-
+helpFunc()
 window.mainloop()
--- Task2/server.py
+++ Task2/server.py
@@ -1,6 +1,5 @@
 from socket import AF_INET, socket, SOCK_STREAM  
 import sys
-import time
 from threading import Thread
 import logging
 
@@ -8,11 +7,13 @@ usernames=[]
 
 def sendUser(msg,user,name):
     sent=False
+    #send message to requested recipient
     for username in usernames:
         if username[0]==user:
             username[1].send("{}".format(msg).encode())
             sent=True
-            logging.info(msg+" whispered to"+ user) 
+            logging.info(msg+" whispered to "+ user) 
+    #send error message back to user
     if not sent:
         for username in usernames:
             if username[0]==name:     
@@ -28,62 +29,71 @@ def accept_incoming_connections():
         Thread(target=handle_client, args=(client,)).start()          
 
 def handle_client(client):
-   #Handles a single client connection.
-   name = client.recv(buffer_size).decode()
-   usernames.append([name,client])
-   clients[client] = name
-   broadcast("{} has joined".format(name))
-   while True:
+    #Handles a single client connection.
+    name = client.recv(buffer_size).decode()
+    usernames.append([name,client])
+    clients[client] = name
+    broadcast("{} has joined".format(name))
+    while True:
         try:
             msg = client.recv(buffer_size).decode()
+            #user has requested to change username
             if msg[:4] == "USER":
                 user=clients[client]
                 newName=msg[4:]
                 clients[client]=newName
                 for u in range(len(usernames)):
-                    if usernames[u]==user:
-                        usernames[u]=newName
+                    if usernames[u][0]==user:
+                        usernames[u]=[newName,usernames[1]]
                 broadcast(user+" has changed their name to "+newName) 
-                logging.info(user+" has changed their name to "+newName) 
+            #user has requested to see a list of all users
             elif msg=="DISPLAY":
                 for i in range(len(usernames)):
                     msg=usernames[i][0] 
                     client.send("{}".format(msg).encode())
+            #user is leaving
             elif msg == "quit":
                 client.send("quit".encode())
                 client.close()
                 print("{} has left.".format(clients[client]))
                 user=clients[client]
-                usernames.remove(user)
+                for u in range(len(usernames)):
+                    if usernames[u][0]==user:
+                        usernames.remove(usernames[u])
                 del clients[client]
+                #inform all users
                 broadcast(user+" has left.")
-                logging.info(user+" has left.")
                 break
+            #send message
             else:
                 msgParts=msg.split('*')
+                #send to all
                 if msgParts[1]=="ALL":
                     broadcast(msgParts[0])
+                #send to one user
                 else:
                     sendUser(msgParts[0],msgParts[1],name)
-
+        #disconnect user if they crash
         except:
             print("{} has left.".format(clients[client]))
             user=clients[client]
             del clients[client]
             broadcast(user+" has left the chat.")
-            logging.info(user+" has left.")
             break
 
+#send to all users
 def broadcast(msg):
     for sock in clients:
         sock.send("{}".format(msg).encode())
     logging.info("{} ".format(msg))
 
-def close_all():
+#if server crasheds, disconnect all users
+def closeAll():
     for sock in clients:
         sock.send("Server has crashed! Disconnecting...".encode())
         sock.close()
     logging.info("server crashed. All users disconneted")
+
 #check sys arguments
 if len(sys.argv) != 2 :
     print ('Incorrect command line arguments given')
@@ -112,5 +122,5 @@ if __name__ == "__main__":
         ACCEPT_THREAD.start()
         ACCEPT_THREAD.join()
     except:
-        close_all()
+        closeAll()
         server.close()
